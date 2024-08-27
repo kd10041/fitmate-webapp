bdb7504017c20ce1a7bb385a17bac60d2c437d0f
Kumar Divyanshu <kumar.divyanshu.iitkgp@gmail.com>
2024-08-27 23:54:00 +0530

    updater server.js with CORS

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,43 @@
-//Importing express 
-const express=require('express');
-//Import dotenv
+// Importing express 
+const express = require('express');
+// Import dotenv
 require('dotenv').config()
 
-//Importing mongoose
-const mongoose=require("mongoose")
+// Importing mongoose
+const mongoose = require("mongoose")
 
-//Importing routes
-const workoutRoutes=require('./routes/workouts')
+// Importing routes
+const workoutRoutes = require('./routes/workouts')
 
-//Express is Nodejs frame work to manage serve easily 
-const app=express();
+// Importing CORS
+const cors = require('cors');
 
-//Middleware handle   request 
+// Express is Node.js framework to manage server easily 
+const app = express();
+
+// Middleware to handle JSON request
 app.use(express.json())
 
-//Middleware
-app.use((req,res,next)=>{
-    console.log(req.path,req.method);
-    next()
+// Enable CORS for all requests
+app.use(cors()); // Allow all origins
+
+// Middleware to log requests
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
 })
 
-//Routes
-app.use('/api/workouts',workoutRoutes);
-//Use Postman to test the request
+// Routes
+app.use('/api/workouts', workoutRoutes);
 
-//Connect to DB
+// Connect to DB
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
-    //Listen for request
-    app.listen(process.env.PORT,()=>{
-    console.log("Connected to DB & listening to Port",process.env.PORT); 
-})
-})
-.catch((error)=>{
-    console.log(error)
-})
-//Use env variable
-
-
+    .then(() => {
+        // Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log("Connected to DB & listening on Port", process.env.PORT); 
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
